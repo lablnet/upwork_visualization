992da5ce4f447d178c3d297b06a9aa5576beb415
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-04-14 12:47:15 +0500

    v1.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/upwork_visualization/',
+  //base: '/upwork_visualization/',
 })
