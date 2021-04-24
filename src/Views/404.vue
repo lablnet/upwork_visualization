853edc84b231d02d173af2d03e4ece87c55bbba5
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-04-24 08:23:58 +0500

    remove unnecessary files

--- src/Views/404.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="text-center mt-5">
-    <h3>404 Not Found!</h3>
-    <p>The page you looking for may deleted or not available.</p>
-    <router-link class="btn btn-info" to="/">Home</router-link>
-  </div>
-</template>
